253f8c149a2ca532229fc487a68175c63dc934af
Meriem Mekhdoul <meriem.mekhdoul@etu.univ-lyon1.fr>
2024-11-13 15:40:56 +0100

    implémentation de getCase et getNbCases (nombre de cases voisines vivantes)

--- modele/Environnement.java
+++ modele/Environnement.java
@@ -5,27 +5,10 @@ import java.util.Map;
 import java.util.Observable;
 
 public class Environnement extends Observable implements Runnable {
-    private Case[][] tab;
-    private Map<Case,Integer> mapDonnees;
-
-    public int getSizeX() {
-        return sizeX;
-    }
-
-    public int getSizeY() {
-        return sizeY;
-    }
-
-    private int sizeX, sizeY;
-
-    public boolean getState(int x, int y) {
-        return tab[x][y].getState();
-    }
-
-    public Case getCase(Case source, Direction d) {
-        // TODO : une case utilisera obligatoirement cette fonction pour percevoir son environnement, et définir son état suivant
-        return null;
-    }
+    private final Case[][] tab;
+    private final Map<Case, Position> mapDonnees;
+    private final int sizeX;
+    private final int sizeY;
 
 
     public Environnement(int _sizeX, int _sizeY) {
@@ -38,24 +21,104 @@ public class Environnement extends Observable implements Runnable {
 
         for (int i = 0; i < sizeX; i++) {
             for (int j = 0; j < sizeY; j++) {
-                Case c = new Case();
+                Case c = new Case(this);
                 tab[i][j] = c;
-                mapDonnees.put(c,i);
+                mapDonnees.put(c, new Position(i, j));
             }
-
         }
+    }
 
+    public int getSizeX() {
+        return sizeX;
+    }
+
+    public int getSizeY() {
+        return sizeY;
     }
 
     public void rndState() {
         for (int i = 0; i < sizeX; i++) {
             for (int j = 0; j < sizeY; j++) {
                 tab[i][j].rndState();
-
             }
         }
 
     }
+
+    public boolean getState(int x, int y) {
+        return tab[x][y].getState();
+    }
+
+    public int getNbCases(Case source) {
+        int somme = 0;
+        for (Direction d : Direction.values()) {
+            if (getCase(source, d).getState()) {
+                somme++;
+            }
+        }
+        return somme;
+    }
+
+    /**
+     * Rechercher la case voisine de la case source selon la direction demandée, en ne dépassant pas les limites du tableau
+     * @param source la case initiale qui fait appel à cette fonction
+     * @param d la direction de la case voisine
+     * @return la case voisine
+     */
+
+    public Case getCase(Case source, Direction d) {
+        Position pos = mapDonnees.get(source);
+        int x = pos.getX();
+        int y = pos.getY();
+
+        //vérifier si la position n'est pas en dehors du tableau
+        switch (d) {
+            case h -> {
+                if (y > 0)
+                    return tab[x][y - 1];
+                throw new IndexOutOfBoundsException();
+            }
+            case hd -> {
+                if (y > 0 && (x < sizeX - 1))
+                    return tab[x + 1][y - 1];
+                throw new IndexOutOfBoundsException();
+            }
+            case d -> {
+                if (x < sizeX - 1)
+                    return tab[x + 1][y];
+                throw new IndexOutOfBoundsException();
+            }
+            case db -> {
+                if (x < sizeX - 1 && (y < sizeY - 1))
+                    return tab[x + 1][y + 1];
+                throw new IndexOutOfBoundsException();
+            }
+            case b -> {
+                if (y < sizeY - 1)
+                    return tab[x][y + 1];
+                throw new IndexOutOfBoundsException();
+            }
+            case bg -> {
+                if (x > 0 && (y < sizeY - 1))
+                    return tab[x - 1][y + 1];
+                throw new IndexOutOfBoundsException();
+            }
+            case g -> {
+                if (x > 0)
+                    return tab[x - 1][y];
+                throw new IndexOutOfBoundsException();
+            }
+            case gh -> {
+                if (x > 0 && y > 0)
+                    return tab[x - 1][y - 1];
+                throw new IndexOutOfBoundsException();
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
 
     @Override
     public void run() {
