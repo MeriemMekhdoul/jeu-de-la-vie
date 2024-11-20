a5af4af836b04c882431ba4c84410c1f8d28f18d
Meriem Mekhdoul <meriem.mekhdoul@etu.univ-lyon1.fr>
2024-11-20 19:44:50 +0100

    séparation du fonctionnement en 2 mode : mode manuel (click clavier) et mode automatique (boucle jusqu'à interruption

--- modele/Ordonnanceur.java
+++ modele/Ordonnanceur.java
@@ -1,6 +1,7 @@
 package modele;
 
 import java.awt.*;
+import java.util.Scanner;
 
 import static java.lang.Thread.*;
 
@@ -8,20 +9,55 @@ public class Ordonnanceur extends Thread {
 
     private long sleepTime;
     private Runnable runnable;
+    private boolean exit = false;
+    private boolean modeAuto = false;
+
     public Ordonnanceur(long _sleepTime, Runnable _runnable) {
         sleepTime = _sleepTime;
         runnable = _runnable;
     }
 
+    public boolean getModeAuto(){
+        return modeAuto;
+    }
+    public void setExit(boolean etat){
+        exit = etat;
+    }
+    @Override
     public void run() {
-        while (true) {
-            runnable.run();
-            try {
-                sleep(sleepTime);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        while (!exit) {
+            if (modeAuto) {
+                // Exécution en mode automatique
+                runAlone();
+            } else {
+                // Mode manuel attend les entrées via FenetrePrincipale
+                try {
+                    Thread.sleep(100); // Petite pause pour éviter la surcharge CPU
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
             }
+        }
+        System.out.println("Exécution terminée.");
+        //TODO: exit le programme ? car maintenant on peut toujours relancer avec ESPACE..
+    }
+
+    public void runManuel() {
+        modeAuto = false;
+        runnable.run(); // Exécute une tâche unique
+    }
+
+    public void runAlone() {
+        runnable.run();
+        try {
+            Thread.sleep(sleepTime); // Pause entre chaque exécution
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 
+
+    public void setModeAuto(boolean etat) {
+        modeAuto = etat;
+    }
 }
