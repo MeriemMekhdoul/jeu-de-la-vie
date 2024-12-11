3d72128b0cf3c0ad693ce1f69b969ccd81d5a460
Meriem Mekhdoul <meriem.mekhdoul@etu.univ-lyon1.fr>
2024-12-11 03:13:18 +0100

    chargement des motifs prédéfinis dans la liste au lancement de l'app à partir du dossier spécifié - mettre le chelin vers le dossier des données du jeu en constante statique

--- modele/Simulateur.java
+++ modele/Simulateur.java
@@ -1,11 +1,14 @@
 package modele;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Simulateur {
     public static int simulationSpeed = 550; // Valeur par défaut
     private Environnement env;
     private Ordonnanceur ord;
+    public final static String DIRECTORY_PATH = System.getProperty("user.home") + "\\JeuDeLaVie"; //TODO: maybe changer le chemin ?
 
     public Simulateur(Environnement _env, Ordonnanceur _ord){
         env = _env;
@@ -61,7 +64,7 @@ public class Simulateur {
     public void sauvegarderEcran(Position p1, Position p2) throws IOException {
         //sauvegarder l'etat actuel de la grille
         // Chemin du répertoire
-        File directory = new File(System.getProperty("user.home") + "\\JeuDeLaVie");//TODO: maybe changer le chemin ?
+        File directory = new File(DIRECTORY_PATH);
 
         // Vérifie si le répertoire existe, sinon le crée
         if (!directory.exists()) {
@@ -93,7 +96,7 @@ public class Simulateur {
     }
 
     public Environnement chargerEcran() throws IOException {
-        File file= new File(System.getProperty("user.home") + "\\JeuDeLaVie\\screenSauvegarde.bin");//TODO: mettre en constante
+        File file= new File(DIRECTORY_PATH + "\\screenSauvegarde.bin");
         Environnement en = null;
 
         if(file.exists()) {
@@ -124,5 +127,72 @@ public class Simulateur {
 
     public void setSousEnv(Environnement sEnv, Position p){
         env.setSousEnv(sEnv, p);
+    }
+
+    //TODO: enlever les messages de debug
+    public List<Environnement> chargerEnvironnements() {
+        List<Environnement> environnements = new ArrayList<>();
+
+        // Définir le chemin du répertoire des motifs
+        String homePath = DIRECTORY_PATH + "\\motifs"; // Supposons que 'motifs' est un dossier à créer
+        File directory = new File(homePath);
+
+        // Vérifier si le dossier existe
+        System.out.println("Vérification du dossier: " + homePath);
+        if (directory.exists() && directory.isDirectory()) {
+            System.out.println("Le dossier existe et contient des fichiers.");
+
+            // Parcourir les fichiers du dossier
+            File[] files = directory.listFiles();
+            if (files != null) {
+                System.out.println("Nombre de fichiers trouvés: " + files.length);
+                for (File file : files) {
+                    if (file.isFile()) {
+                        // Charger chaque environnement depuis le fichier
+                        System.out.println("Chargement du fichier: " + file.getName());
+                        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+                            Environnement env = (Environnement) ois.readObject();
+                            System.out.println("Environnement chargé depuis le fichier: " + file.getName());
+
+                            // Affichage du sous-environnement pour debug
+                            System.out.println("Sous-environnement extrait de " + file.getName() + ":");
+                            for (int i = 0; i < env.getSizeX(); i++) {
+                                for (int j = 0; j < env.getSizeY(); j++) {
+                                    System.out.print(env.getCase(i, j).getState() ? "1 " : "0 ");
+                                }
+                                System.out.println();
+                            }
+
+                            // Ajouter l'environnement à la liste
+                            environnements.add(env);
+                        } catch (IOException e) {
+                            System.err.println("Erreur lors de la lecture du fichier " + file.getName());
+                            e.printStackTrace();
+                        } catch (ClassNotFoundException e) {
+                            System.err.println("Classe introuvable lors du chargement de " + file.getName());
+                            e.printStackTrace();
+                        }
+                    } else {
+                        System.out.println("Le fichier n'est pas valide : " + file.getName());
+                    }
+                }
+            } else {
+                System.out.println("Aucun fichier trouvé dans le dossier.");
+            }
+        } else {
+            // Le dossier n'existe pas ou n'est pas un répertoire valide
+            System.out.println("Le dossier n'existe pas ou n'est pas un répertoire valide. Je vais créer le dossier.");
+            boolean created = directory.mkdirs(); // Crée le dossier "motifs"
+            if (created) {
+                System.out.println("Le dossier 'motifs' a été créé avec succès.");
+            } else {
+                System.out.println("Erreur lors de la création du dossier 'motifs'.");
+            }
+        }
+
+        // Afficher la liste des environnements chargés
+        System.out.println("Environnements chargés: " + environnements.size());
+
+        return environnements;
     }
 }
